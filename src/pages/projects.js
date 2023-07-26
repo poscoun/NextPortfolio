276b0d82a01ef6be3b2c077ec78b065b60a6e9fc
poscoun <poscoun@gmail.com>
2023-07-26 21:08:50 +0900

    projects.js file modify

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,8 +11,8 @@ export default function Projects({projects}) {
         <Layout >
             <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
                 <Head>
-                    <title>빡코딩 포트폴리오</title>
-                    <meta name="description" content="오늘도 빡코딩!" />
+                    <title>전진영 포트폴리오</title>
+                    <meta name="description" content="매일매일 성장하는 개발자" />
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <h1 className="text-4xl font-bold sm:text-6xl">
