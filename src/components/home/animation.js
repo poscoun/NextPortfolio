ba6d5546167bdefb79cbdc22b005dcc6abc50195
jinyeong.jun <poscoun@gmail.com>
2025-04-28 13:13:52 +0900

    [2025-04-28 전진영] 서버사이드 렌더링 비활성화하도록 코드 수정

--- src/components/home/animation.js
+++ src/components/home/animation.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
+import dynamic from 'next/dynamic';
+import lottieJson from '/public/animation.json';
 
-import Lottie from 'react-lottie-player'
-// Alternatively:
-// import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
-
-import lottieJson from '/public/animation.json'
+// Lottie 컴포넌트를 dynamic import로 불러옴 (서버사이드 렌더링 비활성화)
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
 
 export default function Animation() {
   return (
-    <Lottie
-      loop
-      animationData={lottieJson}
-      play
-    />
-  )
+      <Lottie
+          loop
+          animationData={lottieJson}
+          play
+          //style={{ width: 300, height: 300 }} // 스타일은 필요에 따라 조정 가능
+      />
+  );
 }
